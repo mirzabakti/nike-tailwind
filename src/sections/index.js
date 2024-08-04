241941abe9cb0e8700b940056989133467a2890f
mirzabakti <mirza.bakti@gmail.com>
2024-08-05 02:25:14 +0700

    Membuat components dan sections lalu import export

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+
+export { Hero, Footer, CustomerReviews, SpecialOffer, Services, PopularProducts, SuperQuality, Subscribe };
