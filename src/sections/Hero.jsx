c0e3dd2c0043e589490500c1d876cd4d07a45dde
mirzabakti <mirza.bakti@gmail.com>
2024-08-05 10:28:07 +0700

    membuat dasaran statistics

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '../components/Button'
 import { arrowRight } from '../assets/icons'
+import { statistics } from '../constants'
 
 const Hero = () => {
   return (
@@ -20,6 +21,14 @@ const Hero = () => {
           Discover stylish Nike arrivals, quality comfort, and innovation for your active life.
         </p>
         <Button label="Shop Now" iconURL={arrowRight} />
+      </div>
+      <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+        {statistics.map((stat) => (
+          <div key={stat.label}>
+            <p>{stat.value}</p>
+            <p>{stat.label}</p>
+          </div>
+        ))}
       </div>
     </section>
   )
