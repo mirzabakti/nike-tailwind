c9f42e066908deb32713507ca47012e703ec06bb
mirzabakti <mirza.bakti@gmail.com>
2024-08-05 10:56:27 +0700

    styling hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,14 +10,13 @@ const Hero = () => {
       className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
     >
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-lg:padding-x pt-28'>
-        <p>Our Summer Collection</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p className='text-xl font-montserrat text-coral-red'>Our Summer Collection</p>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival</span>
           <br />
-          <span>Nike</span>
-          Shoes
+          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
         </h1>
-        <p>
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
           Discover stylish Nike arrivals, quality comfort, and innovation for your active life.
         </p>
         <Button label="Shop Now" iconURL={arrowRight} />
