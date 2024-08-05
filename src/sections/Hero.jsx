aec21a6e1e83e3281a737acbf49dedfb5eafb002
mirzabakti <mirza.bakti@gmail.com>
2024-08-05 11:11:48 +0700

    styling statistics

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       id="home"
       className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
     >
-      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-lg:padding-x pt-28'>
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-coral-red'>Our Summer Collection</p>
         <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
           <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival</span>
@@ -24,8 +24,8 @@ const Hero = () => {
       <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
         {statistics.map((stat) => (
           <div key={stat.label}>
-            <p>{stat.value}</p>
-            <p>{stat.label}</p>
+            <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
+            <p className='leading-7 font-montserrat text-slate-gray'>{stat.label}</p>
           </div>
         ))}
       </div>
